d9378488b3a335417aaaca64c0d885fe63978714
Pratiksha <Pratiksha@codevergeit.com>
2026-06-23 12:22:49 +0530

    add

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -379,12 +379,12 @@ export default function Careers() {
                     {isSubmitting ? (
                       <>
                         <div className="w-5 h-5 border-2 border-white/30 border-t-white rounded-full animate-spin" />
-                        SUBMITTING...
+                        Submitting...
                       </>
                     ) : (
                       <>
                         {/* <Send size={16} /> */}
-                        SUBMIT APPLICATION
+                        Submit Application
                       </>
                     )}
                   </button>
